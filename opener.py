ccbadd55f0b5b65d7a28add9d139f9d276ca9b82
masvel <maslov.pa@phystech.edu>
2023-07-31 16:13:55 +0300

    Added a file to open browser

---
+++ opener.py
@@ -0,0 +1,3 @@
+#webbrowser module helps in open default web browser of os
+from webbrowser import open
+open("https://yandex.ru/maps/10827/donskoy/?ll=38.336979%2C53.968442&mode=search&sll=38.337383%2C53.968340&text=53.968340%2C38.337383&z=18.55")
